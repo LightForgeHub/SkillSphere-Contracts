1a4ddef34b3ef5b8e479d4be034039d947926314
bosunUbuntu <joshuaibitoye111@gmail.com>
2026-03-27 15:43:21 +0100

    empty types file

--- contracts/reputation-scoring-contract/src/types.rs
+++
@@ -1,9 +0,0 @@
-use soroban_sdk::contracttype;
-
-#[contracttype]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ExpertReputation {
-    pub total_sessions: u64,
-    pub cumulative_score: u64,
-    pub updated_at: u64,
-}
